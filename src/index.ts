688390d375f7ed66b704256f69ed7016c4686789
KarbonKitty <308lapua.magnum@gmail.com>
2018-04-13 09:41:08 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Vue from "vue";
+
+let vm = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});
