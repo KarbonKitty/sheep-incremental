5ab9f375eb71d1abb0443e276ec736f24c0e4b31
KarbonKitty <308lapua.magnum@gmail.com>
2018-04-24 08:00:24 +0200

    Add new filter for per-second values

--- src/filters.ts
+++ src/filters.ts
@@ -2,6 +2,26 @@ function decimal(value: number, numberOfDigits: number) {
   return value.toFixed(numberOfDigits);
 }
 
+function signed(value: number) {
+  if (value >= 0) {
+    return `+${value}`;
+  } else {
+    // negative numbers have a minus in front of them by default
+    return `${value}`;
+  }
+}
+
+function signedDecimal(value: number, precision: number) {
+  if (value >= 0) {
+    return `+${value.toFixed(precision)}`;
+  } else {
+    // negative numbers have a minus in front of them by default
+    return `${value.toFixed(precision)}`;
+  }
+}
+
 export default {
-  decimal
+  decimal,
+  signed,
+  signedDecimal
 }
