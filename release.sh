3bbda0111f5ad3129ee8d102f6aad4e0da1083f2
KarbonKitty <308lapua.magnum@gmail.com>
2018-05-10 19:53:24 +0200

    Basic release script

---
+++ release.sh
@@ -0,0 +1,6 @@
+webpack --mode=production
+rm -rf ../temp-sheep-incremental
+mkdir ../temp-sheep-incremental
+cp -r dist ../temp-sheep-incremental
+cp index.html ../temp-sheep-incremental
+cp site.css ../temp-sheep-incremental
