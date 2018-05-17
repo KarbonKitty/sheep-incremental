29f1cf48e4d68dbd694eac5c1c15bbd1d5f17529
KarbonKitty <308lapua.magnum@gmail.com>
2018-05-17 17:54:47 +0200

    Add more type guards

--- src/classes/typeGuards.ts
+++ src/classes/typeGuards.ts
@@ -2,6 +2,8 @@ import Producer from "./producer/Producer";
 import Discovery from "./discovery/Discovery";
 import GameObject from "./gameObject/GameObject";
 import IBuyable from "./IBuyable";
+import Upgrade from "./upgrade/Upgrade";
+import Storage from "./storage/Storage";
 
 export default {
     isProducer(gameObject: GameObject): gameObject is Producer {
@@ -12,5 +14,11 @@ export default {
     },
     isBuyable(gameObject: Object): gameObject is IBuyable {
         return typeof (<IBuyable>gameObject).buy !== 'undefined';
+    },
+    isStorage(gameObject: Object): gameObject is Storage {
+        return (<Storage>gameObject).type === 'storage';
+    },
+    isUpgrade(gameObject: Object): gameObject is Upgrade {
+        return (<Upgrade>gameObject).type === 'upgrade';
     }
 }
