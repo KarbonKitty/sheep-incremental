7d740827b112dcaa4349be38dfb51e89daadc10a
KarbonKitty <308lapua.magnum@gmail.com>
2018-05-20 19:26:01 +0200

    Add second temp goal

--- src/data/goals.ts
+++ src/data/goals.ts
@@ -1,7 +1,8 @@
 import { Map, Price } from "../classes/baseClasses";
 
 let goals: Map<Price> = {
-    tribal: { herbs: 1 }
+    tribal: { herbs: 1 },
+    copper: { herbs: 2 }
 }
 
 export default goals;
