fcde69f56b98e47735e038e9e05d9af6ea6b0730
KarbonKitty <308lapua.magnum@gmail.com>
2018-05-22 08:50:28 +0200

    Second goal fleshed out

--- src/data/goals.ts
+++ src/data/goals.ts
@@ -2,7 +2,7 @@ import { Map, Price } from "../classes/baseClasses";
 
 let goals: Map<Price> = {
     tribal: { bread: 150, meat: 250, beer: 100 },
-    copper: { herbs: 2 }
+    copper: { pottery: 200, bread: 200, meat: 300, beer: 150 }
 }
 
 export default goals;
