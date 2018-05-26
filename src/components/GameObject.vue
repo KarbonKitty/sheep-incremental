fb10d2eb4c815e62f23256b4e446bc060769b4e1
KarbonKitty <308lapua.magnum@gmail.com>
2018-05-26 16:55:18 +0200

    UI improvements
    The gameObject button is styled depending on whether or not player can afford to pay for the object in question

--- src/components/GameObject.vue
+++ src/components/GameObject.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
-  div(v-if="gameObject.locks.length === 0 && !gameObject.done" class="gameObject" @click="changeSelection")
+  div(v-if="gameObject.locks.length === 0 && !gameObject.done" class="gameObject" @click="changeSelection" :class="{ available: canBeBought }")
     p.
-      #[strong {{ gameObject.name }} #[span(v-if="typeof gameObject.quantity === 'number'") ({{ gameObject.quantity }})] ]
+      {{ gameObject.name }} #[span(v-if="typeof gameObject.quantity === 'number'") ({{ gameObject.quantity }})]
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import EventBus from "../eventBus";
 import GameObject from "../classes/gameObject/GameObject";
+import { IResourcesData } from "../classes/baseClasses";
 
 export default Vue.extend({
   methods: {
@@ -16,7 +17,13 @@ export default Vue.extend({
     }
   },
   props: {
-    gameObject: Object as () => GameObject
+    gameObject: Object as () => GameObject,
+    resources: Object as () => IResourcesData
+  },
+  computed: {
+    canBeBought: function(): boolean {
+        return Object.keys(this.gameObject.currentPrice).reduce((acc, cur) => acc && this.resources[cur].amount >= (this.gameObject.currentPrice[cur] || 0), true);
+    }
   }
 });
 </script>
@@ -34,5 +41,11 @@ export default Vue.extend({
 
   .gameObject > p {
     margin: 0.5rem 0;
+  }
+
+  .available {
+    font-weight: bold;
+    background-color: #073642;
+    /* color: #93a1a1; */
   }
 </style>
