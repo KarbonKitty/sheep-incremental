0bc35c46b457bdfb9700d60eba4a8c9c6259f6a2
KarbonKitty <308lapua.magnum@gmail.com>
2018-07-19 17:57:45 +0200

    Add warning for full storage

--- src/components/Resource.vue
+++ src/components/Resource.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   p(v-if="resource.locks.length === 0").
-    {{ resource.template.name | capitalize }}: {{ resource.amount | decimal(resource.template.precision) }} #[span(v-if="resource.limit != null") / {{ resource.limit | decimal(resource.template.precision) }}] #[span(:class="{ warning: resource.gainPerSecond < 0 }") ({{ resource.gainPerSecond | signedDecimal(resource.template.precision) }} / sec)]
+    {{ resource.template.name | capitalize }}: {{ resource.amount | decimal(resource.template.precision) }} #[span(v-if="resource.limit != null") / {{ resource.limit | decimal(resource.template.precision) }}] #[span(:class="{ negative: resource.gainPerSecond < 0 }") ({{ resource.gainPerSecond | signedDecimal(resource.template.precision) }} / sec)] #[span(v-if="resource.limit * 0.95 <= resource.amount").storageWarning ⚠]
 </template>
 
 <script lang="ts">
@@ -19,8 +19,12 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-  .warning {
+  .negative {
     color: #dc322f;
+  }
+
+  .storageWarning {
+    color: #cb4b16;
   }
 </style>
 
